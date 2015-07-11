7398d0fc062d4d9582bdd6f306f06338751d3f2b
ivanov_nickolay <ivanov.nickolay@gmail.com>
2015-07-11 16:29:35 +0300

    Настройка классов для доступа к базе данных

--- Lib/DB/DB.php
+++ Lib/DB/DB.php
@@ -1,14 +1,25 @@
 <?php
 require_once ("./Lib/Config/ConfigSingleton.php");
+require_once ("../Exception/ExceptionBase.php");
 /**
- * Класс подключается к базе данных и вохвращает экземпляр 
- *
+ * Класс подключается к базе данных и возвращает экземпляр 
+ * класс построен по паттерну одиночка 
+ * всегда есть только одно подключение к базе данных 
  * @author Администратор
  */
 class DB {
-    function __construct() 
+    
+    private static $instance;
+    
+    /**     
+     * закрытый конструктор для подключения к базе данных 
+     * @return \mysqli
+     * @throws ExceptionBase
+     */
+    private function __construct() 
     {
         $Conf = ConfigSingleton::init();
+        
         $host = $Conf->getValue('DB','host');
         $user = $Conf->getValue('DB','username');
         $password= $Conf->getValue('DB','passwd');
@@ -18,8 +29,20 @@ class DB {
         $MySql = new mysqli($host, $user, $password, $database, $port, $socket);
         if ($MySql->connect_error) 
         {
-        die('Ошибка подключения (' . $MySql->connect_errno. ') '. $MySql->connect_error);
+            throw new ExceptionBase("Ошибка подключения к Базе данных");
         }
         return $MySql; 
     }
+    /**
+     * Статическая функция которая возвращает подключение к базе данных
+     * @return ссылку на подключение к база данных 
+     */
+    public static function DB()
+   {
+          if ( empty( self::$instance ) ) {
+            self::$instance = new DB ();
+        }
+        return self::$instance;
+   }
+            
 }
